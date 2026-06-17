9d176f239ed48d0766c63448303d29fe4b7ed42e
klewy <cigankov807@gmail.com>
2026-06-18 08:58:45 +1000

    avoid mem allocation

--- nf_module/kernelspace/fwlish_main.c
+++ nf_module/kernelspace/fwlish_main.c
@@ -13,7 +13,7 @@
 #include <linux/string.h>
 #include "fwlish_nl.h"
 
-static struct nf_hook_ops* nf_ops = NULL;
+
 static bool should_pass(const struct iphdr* ip, const struct tcphdr* tcp) {
     for (int i = 0; i < sizeof(ips) / sizeof(ips[0]); ++i) {
         const uint32_t ip_be = ips[i];
@@ -35,19 +35,16 @@ static unsigned int fw_hook(void *priv, struct sk_buff *skb, const struct nf_hoo
     return NF_ACCEPT;
 }
 
+
+static struct nf_hook_ops nf_ops = {
+    .pf = PF_INET,
+    .hooknum = NF_INET_PRE_ROUTING,
+    .priority = -100,
+    .hook = fw_hook
+};
+
 static int __init fw_init(void) {
-    nf_ops = (struct nf_hook_ops*)kzalloc(sizeof(struct nf_hook_ops), GFP_KERNEL);
-    if (unlikely(!nf_ops)) {
-        pr_alert("Could not allocate nfhookops\n");
-        return -1;
-    }
-
-    nf_ops->pf = PF_INET;
-    nf_ops->hooknum = NF_INET_PRE_ROUTING;
-    nf_ops->priority = -100;
-    nf_ops->hook = fw_hook;
-
-    int ret = nf_register_net_hook(&init_net, nf_ops);
+    int ret = nf_register_net_hook(&init_net, &nf_ops);
     if (unlikely(ret)) {
         pr_err("nf_register_net_hook failed\n");
         return -1;
@@ -55,19 +52,14 @@ static int __init fw_init(void) {
 
     ret = genl_register_family(&fwlish_fam);
     if (unlikely(ret)) {
-        pr_alert("genl_register_family failed\n");
+        pr_err("genl_register_family failed\n");
         return -1;
     }
-
     return 0;
 }
 
 static void __exit fw_deinit(void) {
-    if (likely(nf_ops)) {
-        nf_unregister_net_hook(&init_net, nf_ops);
-        kfree(nf_ops);
-    }
-
+    nf_unregister_net_hook(&init_net, &nf_ops);
     genl_unregister_family(&fwlish_fam);
 }
 
